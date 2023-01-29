b8208bf63bda2f6c5dab213bd696a9aef064aea0
jjusticz <jjusticz@uchicago.edu>
2023-01-29 23:35:02 +0000

    Second commit

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -21,4 +21,18 @@ class CurrenciesController < ApplicationController
 
   end
 
+  def exchange
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @currencies = @parsed_data.fetch("symbols").keys
+    @from_cur = params.fetch("from_currency")
+    @to_cur = params.fetch("to_currency")
+    @exchange_raw = open("https://api.exchangerate.host/convert?from=#{@from_cur}&to=#{@to_cur}")
+    @exchange_parsed = JSON.parse(@exchange_raw)
+    @exchange_rate = @exchange_parsed.fetch("result")
+
+    render({:template => "layouts/step_three.html.erb"})
+
+  end
+
 end
